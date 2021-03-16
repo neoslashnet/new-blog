48d81a7074d6741c1e7280891cb43c12980520ec
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-15 17:42:36 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,10 +7,7 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />          
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
