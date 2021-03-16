d45956f3afdd80d99167ce9605d4ff346bb3e0c7
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-15 21:37:25 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -22,8 +22,8 @@ export default function About() {
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
               {siteMetadata.author}
             </h3>
-            <div className="text-gray-500 dark:text-gray-400">Professor of Atmospheric Science</div>
-            <div className="text-gray-500 dark:text-gray-400">Stanford University</div>
+            <div className="text-gray-500 dark:text-gray-400">Senior IT Systems Engineer</div>
+            <div className="text-gray-500 dark:text-gray-400">Infrastructure, Cybersecurity, AWS, Azure, Networking, Consulting</div>
             <div className="flex pt-6 space-x-3">
               <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />                          
               <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
@@ -32,9 +32,8 @@ export default function About() {
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
             <p>
-              Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His
-              research interests includes complexity modelling of tailwinds, headwinds and
-              crosswinds.
+              Hello there!
+              My name name is David Galiata.
             </p>
             <p>
               He leads the clean energy group which develops 3D air pollution-climate models, writes
