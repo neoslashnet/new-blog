6f0d3bd5595a71440e4ae98b6d50ca623637419a
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-16 12:18:37 -0700

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,12 +4,16 @@ const projectsData = [
     description: `What is you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
-    
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
   },
   {
     title: 'The Time Machine',
-    description: `Completed projects,
-    
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten handphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
   },
 ]
 
