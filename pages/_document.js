45b65f3055fa40999621cfc02bcde2b5b0a6d374
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-21 09:21:25 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -78,14 +78,19 @@ class MyDocument extends Document {
     async
     src={`https://www.googletagmanager.com/gtag/js?id=${169236737-1}`}
   />
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-169236737-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-169236737-1');
-</script>
+  <script
+    // eslint-disable-next-line react/no-danger
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${169236737-1}', {
+          page_path: window.location.pathname,
+        });
+      `,
+    }}
+  />
 
         </Head>
         <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
