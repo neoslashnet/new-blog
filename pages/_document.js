2d6fdffb827da48cabeedafb0765fe167bd53b1b
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-21 09:17:03 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -78,7 +78,6 @@ class MyDocument extends Document {
     async
     src={`https://www.googletagmanager.com/gtag/js?id=${169236737-1}`}
   />
-  <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169236737-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
