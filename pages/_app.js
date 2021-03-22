ab9fdb9e7e1707199f428e4f40d26259c4f729fd
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-21 21:03:40 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,22 +10,7 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import MDXComponents from '@/components/MDXComponents'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import * as gtag from '/lib/gtag'
-
-const App = ({ Component, pageProps }) => {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
-  return <Component {...pageProps} />
-}
+import * as gtag from 'lib/gtag'
 
 export default function App({ Component, pageProps }) {
   return (
