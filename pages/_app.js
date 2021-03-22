024b0b5af3ecf0fb16a8d67d80683a1cf3b9e982
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-21 20:56:26 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -21,26 +21,6 @@ export default function App({ Component, pageProps }) {
           <Component {...pageProps} />
         </LayoutWrapper>
       </MDXProvider>
-    </ThemeProvider>
-    import { useEffect } from 'react'
-    import { useRouter } from 'next/router'
-    import * as gtag from '../lib/gtag'
-    
-    const App = ({ Component, pageProps }) => {
-      const router = useRouter()
-      useEffect(() => {
-        const handleRouteChange = (url) => {
-          gtag.pageview(url)
-        }
-        router.events.on('routeChangeComplete', handleRouteChange)
-        return () => {
-          router.events.off('routeChangeComplete', handleRouteChange)
-        }
-      }, [router.events])
-    
-      return <Component {...pageProps} />
-    }
-    
-    export default App
+    </ThemeProvider>  
   )
 }
