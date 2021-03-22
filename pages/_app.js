ed5d506f14e5b7c5924fcf84cb063059f4259848
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-21 21:01:17 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,24 @@ import Head from 'next/head'
 import { SEO } from '@/components/SEO'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import MDXComponents from '@/components/MDXComponents'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '/lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
 
 export default function App({ Component, pageProps }) {
   return (
