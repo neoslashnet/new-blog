e1a00c874d726a3d8211bf76ace7befdb25e0eab
David Galiata neoslash.net <63625999+neoslashnet@users.noreply.github.com>
2021-03-21 20:44:09 -0700

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -24,3 +24,22 @@ export default function App({ Component, pageProps }) {
     </ThemeProvider>
   )
 }
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
